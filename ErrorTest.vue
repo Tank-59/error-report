a4dd8d8d25a70398349eb5ab5a2ec1f4293d9a77
CaoYanqi <caoyanqi15@126.com>
2019-12-24 13:46:34 +0800

    add files

---
+++ ErrorTest.vue
@@ -0,0 +1,60 @@
+<!--错误测试-->
+<template>
+  <div class="error-test" @click="opps">
+    <h2>Error Testing ...</h2>
+    <button>oops!</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ErrorTestPage',
+  data () {
+    return {}
+  },
+  mounted () {
+    this.throwErrTest();
+    // this.scriptErr();
+  },
+  methods: {
+    opps () {
+      console.log('oops!!');
+
+      this.throwErrTest();
+      // this.scriptErr();
+      // this.imgError();
+      // this.promiseErr();
+    },
+    throwErrTest () {
+      throw new Error('my throw error test');
+    },
+    scriptErr () {
+      const scriptEle = document.createElement('script');
+      scriptEle.src = 'https://www.baidu.com/666.js';
+      document.appendChild(scriptEle);
+    },
+    promiseErr () {
+      Promise.reject('promise error ooooo');
+      return new Promise((resolve, reject) => {
+        reject('promise error ooh!!')
+      })
+    },
+    imgError () {
+      const img = document.createElement('img');
+      img.src = 'https://www.baidu.com/test.jpg';
+      document.appendChild(img);
+    }
+  }
+}
+</script>
+<style scoped>
+  .error-test{
+    width: 100%;
+    /*height: 30px;*/
+    line-height: 30px;
+    text-align: center;
+    background: orangered;
+  }
+  .error-test button{
+    padding: 2px 5px;
+  }
+</style>
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # error-report
 项目错误监控上报demo
+
+
+```javascript
+   import initErrorReport from './tools/ErrorReport/index'
+
+   initErrorReport()
+
+```
